0366eabc54be6137ace78ec92901d310c3638b01
Mashima <52222646+zimashima@users.noreply.github.com>
2020-04-13 15:55:23 -0400

    sorta MVP and a stretch

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,9 +1,11 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
   
-  # TO-DO: add missing code
+  for i in arr:
+    if i == target:
+      return True
 
-   return -1   # not found
+  return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
@@ -15,8 +17,16 @@ def binary_search(arr, target):
   low = 0
   high = len(arr)-1
 
-  # TO-DO: add missing code
+  arr = sorted(arr)
 
+  while low < high:
+    mid = (low + high) // 2
+    if arr[mid] == target:
+      return target
+    elif target < arr[mid]:
+      high = mid
+    else:
+      low = mid+1
   return -1 # not found
 
 
